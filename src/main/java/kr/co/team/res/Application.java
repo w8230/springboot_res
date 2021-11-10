1441a4326a6a3827a969e5df223065d57d339778
kjw1237 <kjw1237@gmail.com>
2021-11-10 15:48:31 +0900

    커밋 테스트

--- src/main/java/kr/co/team/res/Application.java
+++ src/main/java/kr/co/team/res/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+    //김진욱 커밋 테스트
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
