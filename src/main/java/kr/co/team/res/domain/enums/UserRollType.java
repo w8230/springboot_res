cc9cc19cfa348b97961aaa64d0451906c29e6760
JerryPigs <w8230@naver.com>
2021-11-11 15:04:41 +0900

    setting

--- src/main/java/kr/co/team/res/domain/enums/UserRollType.java
+++ src/main/java/kr/co/team/res/domain/enums/UserRollType.java
@@ -1,4 +1,18 @@
 package kr.co.team.res.domain.enums;
 
-public class UserRollType {
+public enum UserRollType {
+    MASTER("마스터"),
+    ADMIN("관리자"),
+    NORMAL("일반"),
+    PARTNERS("협력사");
+
+    final private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    private UserRollType(String name){
+        this.name = name;
+    }
 }
