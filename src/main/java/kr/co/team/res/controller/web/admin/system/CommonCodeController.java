f2addec3cddb9e3adb765a5c31cb1e2ee30f901b
JerryPigs <w8230@naver.com>
2021-11-19 01:32:30 +0900

    asd

--- src/main/java/kr/co/team/res/controller/web/admin/system/CommonCodeController.java
+++ src/main/java/kr/co/team/res/controller/web/admin/system/CommonCodeController.java
@@ -4,16 +4,16 @@ import kr.co.team.res.common.Base;
 import kr.co.team.res.common.annotation.CurrentUser;
 import kr.co.team.res.domain.entity.Account;
 import kr.co.team.res.domain.entity.CommonCode;
+import kr.co.team.res.domain.vo.admin.CommonCodeVO;
 import kr.co.team.res.service.web.admin.CommonCodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -52,6 +52,74 @@ public class CommonCodeController extends Base {
             }
         }
         return retList;
+    }
+
+    @ResponseBody
+    @PostMapping("/api/soulGod/commonCode/load")
+    public CommonCode load(Model model,
+                           @RequestBody CommonCodeVO vo) {
+        CommonCode byId = commonCodeService.findById(vo.getId());
+        return byId;
+    }
+
+    @ResponseBody
+    @PostMapping("/api/soulGod/commonCode/save")
+    public String save(Model model,
+                       @CurrentUser Account account,
+                       @RequestBody CommonCodeVO commonCodeForm,
+                       RedirectAttributes redirectAttributes) {
+
+        commonCodeForm.setRegPsId(account.getLoginId());
+        commonCodeForm.setRegDtm(LocalDateTime.now());
+        commonCodeForm.setUpdPsId(account.getLoginId());
+        commonCodeForm.setUpdDtm(LocalDateTime.now());
+        commonCodeForm.setDelAt("N");
+
+        boolean result = commonCodeForm.getId() == 0
+                ? commonCodeService.insert(commonCodeForm) : commonCodeService.update(commonCodeForm);
+
+        String msg = "fail";
+        if (result) {
+            msg = "ok";
+        } else {
+            msg = "fail";
+        }
+        return msg;
+    }
+
+    @ResponseBody
+    @PostMapping("/api/soulGod/commonCode/delete")
+    public String delete(Model model,
+                         @CurrentUser Account account,
+                         @RequestBody CommonCodeVO vo,
+                         RedirectAttributes redirectAttributes) {
+
+        if (vo.getId() == null) {
+            redirectAttributes.addFlashAttribute("message", "잘못된 접근입니다.");
+            return "redirect:" + "/soulGod/systemCode/list";
+        }
+
+        vo.setUpdPsId(account.getLoginId());
+        vo.setUpdDtm(LocalDateTime.now());
+        vo.setDelAt("Y");
+
+        boolean result = commonCodeService.delete(vo);
+
+        String msg = "fail";
+        if (result) {
+            msg = "ok";
+        } else {
+            msg = "fail";
+        }
+        return msg;
+    }
+
+    @ResponseBody
+    @PostMapping("/api/commonCode/listForUppCdPid")
+    public List<CommonCode> listForUppCdPid(Model model,
+                                            @RequestBody CommonCodeVO vo) {
+        List<CommonCode> list = commonCodeService.menuListForUppCdPid(vo.getPrntCodePid());
+        return list;
     }
 
     private List<Map<String, Object>> getMakeTree(Long uppCdPid, List<CommonCode> subList) {
--- src/main/java/kr/co/team/res/service/web/admin/CommonCodeService.java
+++ src/main/java/kr/co/team/res/service/web/admin/CommonCodeService.java
@@ -1,12 +1,20 @@
 package kr.co.team.res.service.web.admin;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import kr.co.team.res.domain.entity.CommonCode;
+import kr.co.team.res.domain.entity.QCommonCode;
 import kr.co.team.res.domain.repository.CommonCodeRepository;
+import kr.co.team.res.domain.vo.admin.CommonCodeVO;
 import kr.co.team.res.service.web._BaseService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,9 +23,87 @@ import java.util.List;
 public class CommonCodeService extends _BaseService {
 
     private final JPAQueryFactory queryFactory;
+    private final ModelMapper modelMapper;
     private final CommonCodeRepository commonCodeRepository;
 
     public List<CommonCode> findAll() {
         return commonCodeRepository.findAllByDelAtOrderByCodeSno("N");
+    }
+    public CommonCode findById(Long id) {
+        return commonCodeRepository.findById(id).orElseGet(CommonCode::new);
+    }
+    @Transactional
+    public boolean insert(CommonCodeVO form) {
+        try {
+            CommonCode commonCode = modelMapper.map(form, CommonCode.class);
+            commonCodeRepository.save(commonCode);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Transactional
+    public boolean update(CommonCodeVO form) {
+        try {
+            CommonCode commonCode = commonCodeRepository.findById(form.getId()).get();
+            commonCode.setPrntCodePid(form.getPrntCodePid());
+            commonCode.setCodeSno(form.getCodeSno());
+            commonCode.setCodeNm(form.getCodeNm());
+            commonCode.setCodeDsc(form.getCodeDsc());
+            commonCode.setCodeValue(form.getCodeValue());
+            commonCode.setUpdPsId(form.getUpdPsId());
+            commonCode.setUpdDtm(LocalDateTime.now());
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Transactional
+    public boolean delete(CommonCodeVO form) {
+        try {
+
+            CommonCode commonCode = commonCodeRepository.findById(form.getId()).get();
+            commonCode.setUpdPsId(form.getUpdPsId());
+            commonCode.setUpdDtm(LocalDateTime.now());
+            commonCode.setDelAt("Y");
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public List<CommonCode> menuListForUppCdPid(Long prntCodePid) {
+        QCommonCode qCommonCode = QCommonCode.commonCode;
+
+        OrderSpecifier<Integer> orderSpecifier = qCommonCode.codeSno.asc();
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        builder.and(qCommonCode.prntCodePid.eq(prntCodePid));
+        builder.and(qCommonCode.delAt.eq("N"));
+
+        List<CommonCode> results = queryFactory
+                .select(Projections.fields(CommonCode.class,
+                        qCommonCode.id,
+                        qCommonCode.prntCodePid,
+                        qCommonCode.codeSno,
+                        qCommonCode.codeNm,
+                        qCommonCode.codeDsc,
+                        qCommonCode.codeValue,
+                        qCommonCode.regPsId,
+                        qCommonCode.regDtm,
+                        qCommonCode.updPsId,
+                        qCommonCode.updDtm,
+                        qCommonCode.delAt
+                ))
+                .from(qCommonCode)
+                .where(builder)
+                .orderBy(orderSpecifier)
+                .fetch();
+
+        return results;
     }
 }
