bbd227ac6b1d00a9d59d5e14f2777fec850a1259
jangminPark <wkdel12@naver.com>
2021-12-09 15:05:56 +0900

    jsp html변환

--- src/main/java/kr/co/team/res/controller/web/TempPageController.java
+++ src/main/java/kr/co/team/res/controller/web/TempPageController.java
@@ -15,18 +15,33 @@ public class TempPageController extends BaseCont{
         return _tempurl + "search_list";
     }
 
+
+    @RequestMapping("/_temppage/inquiry_details")
+    public String inquiry_details(){
+        return _tempurl + "inquiry_details";
+    }
+
+    //게시물 리스트
+    @RequestMapping("/_temppage/gh_list")
+    public String gh_list() {
+        return _tempurl + "gh_list";
+    }
+
+    //게시물 작성
     @RequestMapping("/_temppage/gh_write")
     public String gh_write(){
         return _tempurl + "gh_write";
     }
 
+    //게시물 상세보기
+    @RequestMapping("/_temppage/gh_details")
+    public String gh_details(){
+        return _tempurl + "gh_details";
+    }
+
+    //게시물 수정
     @RequestMapping("/_temppage/gh_modify")
     public String gh_modify(){
         return _tempurl + "gh_modify";
     }
-    @RequestMapping("/_temppage/inquiry_details")
-    public String inquiry_details(){
-        return _tempurl + "inquiry_details";
-    }
-    
 }
